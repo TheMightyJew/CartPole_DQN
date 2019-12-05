742af86cda38ec76e2572d7e7e4b84748fd36c33
TheMightyJew <38401349+TheMightyJew@users.noreply.github.com>
2019-12-05 19:40:20 +0200

    FINISHED

--- Part3.py
+++ Part3.py
@@ -1,20 +1,15 @@
 import datetime
-
 import numpy as np
 import gym
-import keras.backend.tensorflow_backend as backend
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import Adam
-from keras.callbacks import TensorBoard
-import tensorflow as tf
 from collections import deque
 import time
 import random
 from tqdm import tqdm
 import os
-#from PIL import Image
-#import cv2
+from ModifiedTensorBoard import ModifiedTensorBoard
 
 
 def init_variables():
@@ -25,78 +20,30 @@ def init_variables():
     env = gym.make(ENV_NAME)
     observation_space = env.observation_space.shape[0]
     action_space = env.action_space.n
-
     three_layers = True
-
     currTime = datetime.datetime.now()
     timestr = str(currTime.hour) + "_" + str(currTime.minute) + "_" + str(currTime.second)
-    #DISCOUNT = 0.99 #todo check if this is good
     DISCOUNT = 1
     REPLAY_MEMORY_SIZE = 2000  # How many last steps to keep for model training
     MIN_REPLAY_MEMORY_SIZE = 400  # Minimum number of steps in a memory to start training
     MINIBATCH_SIZE = 32  # How many steps (samples) to use for training
     C = 16  # Terminal states (end of episodes)
     MODEL_NAME = None
-    #MIN_REWARD = -200  # For model save
-    #MEMORY_FRACTION = 0.20
-
     # Environment settings
     EPISODES = 20000
-
     # Exploration settings
     epsilon = 1  # not a constant, going to be decayed
-    #EPSILON_DECAY = 0.99975 #todo check if this is good
     EPSILON_DECAY = 0.9995
-    #MIN_EPSILON = 0.001 #todo check if this is good
     MIN_EPSILON = 0.01
     LEARNING_RATE = 0.001
-
     #  Stats settings
     AGGREGATE_STATS_EVERY = 50  # episodes
     SHOW_PREVIEW = False
-    """
-    # For more repetitive results
-    random.seed(1)
-    np.random.seed(1)
-    tf.set_random_seed(1)
-    """
+
 
 # Create models folder
 if not os.path.isdir('models'):
     os.makedirs('models')
-
-
-# Own Tensorboard class
-class ModifiedTensorBoard(TensorBoard):
-
-    # Overriding init to set initial step and writer (we want one log file for all .fit() calls)
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.step = 1
-        self.writer = tf.summary.FileWriter(self.log_dir)
-
-    # Overriding this method to stop creating default log writer
-    def set_model(self, model):
-        pass
-
-    # Overrided, saves logs with our step number
-    # (otherwise every .fit() will start writing from 0th step)
-    def on_epoch_end(self, epoch, logs=None):
-        self.update_stats(**logs)
-
-    # Overrided
-    # We train for one batch only, no need to save anything at epoch end
-    def on_batch_end(self, batch, logs=None):
-        pass
-
-    # Overrided, so won't close writer
-    def on_train_end(self, _):
-        pass
-
-    # Custom method for saving own metrics
-    # Creates writer, writes custom metrics and closes writer
-    def update_stats(self, **stats):
-        self._write_logs(stats, self.step)
 
 
 # Agent class
@@ -111,7 +58,7 @@ class DQNAgent:
         self.target_model.set_weights(self.model.get_weights())
 
         # An array with last n steps for training
-        self.replay_memory = deque(maxlen=REPLAY_MEMORY_SIZE)
+        self.replay_memory = deque(maxlen = REPLAY_MEMORY_SIZE)
 
         # Custom tensorboard object
         self.tensorboard = ModifiedTensorBoard(log_dir="logs/{}-{}".format(MODEL_NAME, int(time.time())))
@@ -157,53 +104,46 @@ class DQNAgent:
         self.replay_memory.append(transition)
 
     # Trains main network every step during episode
-    def train(self, terminal_state, step):
-
+    def train(self, terminal_state):
         # Start training only if certain number of samples is already saved
         if len(self.replay_memory) < MIN_REPLAY_MEMORY_SIZE:
             return
-
         # Get a minibatch of random samples from memory replay table
         minibatch = random.sample(self.replay_memory, MINIBATCH_SIZE)
-
         # Get current states from minibatch, then query NN model for Q values
         current_states = np.array([transition[0] for transition in minibatch])
         current_qs_list = self.model.predict(current_states)
-
         # Get future states from minibatch, then query NN model for Q values
         # When using target network, query it, otherwise main network should be queried
         new_current_states = np.array([transition[3] for transition in minibatch])
         future_qs_list = self.target_model.predict(new_current_states)
-
         X = []
         y = []
-
         # Now we need to enumerate our batches
         for index, (current_state, action, reward, new_current_state, done) in enumerate(minibatch):
-
             # If not a terminal state, get new q from future states, otherwise set it to 0
             # almost like with Q Learning, but we use just part of equation here
             if not done:
-                max_future_q = np.max(future_qs_list[index])
-                new_q = reward + DISCOUNT * max_future_q
+                #PART3
+                max_future_q = np.argmax(current_qs_list[index])
+                q_target_val = future_qs_list[index][max_future_q]
+                new_q = reward + DISCOUNT * q_target_val
+                #PART2
+                #max_future_q = np.max(future_qs_list[index])
+                #new_q = reward + DISCOUNT * max_future_q
             else:
                 new_q = reward
-
             # Update Q value for given state
             current_qs = current_qs_list[index]
             current_qs[action] = new_q
-
             # And append to our training data
             X.append(current_state)
             y.append(current_qs)
-
         # Fit on all samples as one batch, log only on terminal state
         self.model.fit(np.array(X), np.array(y), batch_size=MINIBATCH_SIZE, verbose=0, shuffle=False, callbacks=[self.tensorboard] if terminal_state else None)
-
         # Update target network counter every episode
         if terminal_state:
             self.target_update_counter += 1
-
         # If counter reaches set value, update target network with weights of main network
         if self.target_update_counter > C:
             self.target_model.set_weights(self.model.get_weights())
@@ -221,26 +161,18 @@ def run_program():
     last_100_rewards = deque(maxlen=100)
     max_until_now = -1
     passed_475_before = False
-    """
-    episode = 1
-    while True:
-    """
     # Iterate over episodes
     for episode in tqdm(range(1, EPISODES + 1), ascii=True, unit='episodes'):
         # Update tensorboard step every episode
         agent.tensorboard.step = episode
-
         # Restarting episode - reset episode reward and step number
         episode_reward = 0
         step = 1
-
         # Reset environment and get initial state
         current_state = env.reset()
-
         # Reset flag and start iterating until episode ends
         done = False
         while not done:
-
             # This part stays mostly the same, the change is to query a model for Q values
             if np.random.random() > epsilon:
                 # Get action from Q table
@@ -248,28 +180,16 @@ def run_program():
             else:
                 # Get random action
                 action = np.random.randint(0, action_space)
-
             new_state, reward, done, info = env.step(action)
-
-            # Transform new continous state to new discrete state and count reward
+            # Transform new continues state to new discrete state and count reward
             episode_reward += reward
-
             if SHOW_PREVIEW and not episode % AGGREGATE_STATS_EVERY:
                 env.render()
-
             # Every step we update replay memory and train main network
             agent.update_replay_memory((current_state, action, reward, new_state, done))
-            agent.train(done, step)
-
+            agent.train(done)
             current_state = new_state
             step += 1
-
-        #print("Episode = " + str(episode) + ", Epsilon = " + str(epsilon) + ", Total reward = " + str(episode_reward))
-        # Append episode reward to a list and log stats (every given number of episodes)
-
-        #ep_rewards.append(episode_reward)
-        # look at this after running something good:
-        #todo
         last_100_rewards.append(episode_reward)
         curr_moving_avg = np.average(last_100_rewards)
         agent.tensorboard.update_stats(moving_reward_average=curr_moving_avg, epsilon=epsilon, reward=episode_reward)
@@ -285,17 +205,6 @@ def run_program():
                 agent.model.save(f'models/{MODEL_NAME}__episodes__{EPISODES}__avg__{curr_moving_avg}__min__{int(time.time())}.model')
             else:  # for stopping when reaching 475
                 break
-        """
-        if not episode % AGGREGATE_STATS_EVERY or episode == 1:
-            #average_reward = sum(ep_rewards[-AGGREGATE_STATS_EVERY:])/len(ep_rewards[-AGGREGATE_STATS_EVERY:])
-            #min_reward = min(ep_rewards[-AGGREGATE_STATS_EVERY:])
-            #max_reward = max(ep_rewards[-AGGREGATE_STATS_EVERY:])
-            #agent.tensorboard.update_stats(reward_avg=average_reward, reward_min=min_reward, reward_max=max_reward, epsilon=epsilon)
-    
-            # Save model, but only when min reward is greater or equal a set value
-            if min_reward >= MIN_REWARD:
-                agent.model.save(f'models/{MODEL_NAME}__{max_reward:_>7.2f}max_{average_reward:_>7.2f}avg_{min_reward:_>7.2f}min__{int(time.time())}.model')
-        """
         # Decay epsilon
         if epsilon > MIN_EPSILON:
             epsilon *= EPSILON_DECAY
